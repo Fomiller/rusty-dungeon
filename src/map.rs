aa8d32001d6b2b61e08e4f00af2e91784e08c459
Forrest Miller <forrestmillerj@gmail.com>
2022-08-16 21:14:41 -0500

    implemented player

--- src/map.rs
+++ src/map.rs
@@ -1,4 +1,5 @@
 use crate::prelude::*;
+
 const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
 
 #[derive(Copy, Clone, PartialEq)]
@@ -31,6 +32,22 @@ impl Map {
                     }
                 }
             }
+        }
+    }
+
+    pub fn in_bounds(&self, point: Point) -> bool {
+        point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
+    }
+
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        self.in_bounds(point) && self.tiles[map_idx(point.x, point.y)] == TileType::Floor
+    }
+
+    pub fn try_idx(&self, point: Point) -> Option<usize> {
+        if !self.in_bounds(point) {
+            None
+        } else {
+            Some(map_idx(point.x, point.y))
         }
     }
 }
