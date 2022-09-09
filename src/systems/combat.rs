75f8ac10757e59f09111bc50c3b4a4f1ccc67c55
Forrest Miller <forrestmillerj@gmail.com>
2022-09-09 10:11:06 -0500

    Implemented gameover screen and ability to restart

--- src/systems/combat.rs
+++ src/systems/combat.rs
@@ -2,6 +2,7 @@ use crate::prelude::*;
 
 #[system]
 #[read_component(WantsToAttack)]
+#[read_component(Player)]
 #[write_component(Health)]
 pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
     let mut attackers = <(Entity, &WantsToAttack)>::query();
@@ -10,13 +11,19 @@ pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
         .map(|(entity, attack)| (*entity, attack.victim))
         .collect();
     victims.iter().for_each(|(message, victim)| {
+        let is_player = ecs
+            .entry_mut(*victim)
+            .unwrap()
+            .get_component::<Player>()
+            .is_ok();
+
         if let Ok(mut health) = ecs
             .entry_mut(*victim)
             .unwrap()
             .get_component_mut::<Health>()
         {
             health.current -= 1;
-            if health.current < 1 {
+            if health.current < 1 && !is_player {
                 commands.remove(*victim);
             }
         }
